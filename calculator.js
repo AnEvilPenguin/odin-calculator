2ec542477ef5754b984e3f1955dc9cebd2e56542
AnEvilPenguin <siu06dl@gmail.com>
2024-01-15 17:31:10 +0000

    feat: add basic operations

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// TODO Operation History
+// TODO Track Current Total?
+
+function add(next, current = 0) {
+  return current + next;
+}
+
+function subtract(next, current = 0) {
+  return current - next;
+}
+
+function multiply(next, current = 0) {
+  return current * next;
+}
+
+function divide(next, current = 0) {
+  if (next === 0) {
+    throw new Error("Cannot divide by 0");
+  }
+
+  return current / next;
+}
